7be61427b3c85383158165665eca5a5412e4a966
Josias Santos <josiassantos1577@gmail.com>
2022-04-29 22:42:42 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import logo from './sua-logo-aqui.png';
 import './App.css';
+// import Login from './Login'
 
 function App() {
   return (
-    <div className="App">
+    <>
+     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
+    
+    <div className="login-box">
+    <h2>Login</h2>
+    <form>
+      <div className="user-box">
+        <input type="text" name="" required= { true }/>
+        <label>E-mail</label>
+      </div>
+      <div className="user-box">
+        <input type="password" name="" required= { true }/>
+        <label>Senha</label>
+      </div>
+      <a href="HomePage">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        ENTRAR
+      </a>
+    </form>
+  </div>
+  </>
   );
 }
 
